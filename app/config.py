4784c0c229dc27f90d927c388dd3ab89e64e5c48
devounik <devounik@gmail.com>
2025-02-03 00:07:38 +0530

    Minor changes in config and nlp service

--- app/config.py
+++ app/config.py
@@ -2,3 +2,38 @@
 # Database connection URL
 # Firebase API keys
 # Environment variables
+
+from dotenv import load_dotenv
+import os
+from langchain_huggingface.llms import HuggingFaceEndpoint
+
+
+hf_api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+if not hf_api_key:
+  raise ValueError("Hugging Face Api Key not found in .env file")
+
+hf_repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+hf_temperature = 0.7
+hf_max_new_tokens = 500
+
+def get_llm():
+  return HuggingFaceEndpoint(
+    repo_id=hf_repo_id,
+    temperature=hf_temperature,
+    max_new_tokens=hf_max_new_tokens,
+    model_kwargs={"token":hf_api_key}
+  )
+
+sql_prompt = """
+Convert the following user query into an equivalent MySQL query. 
+The database has a table called employees with the following columns: 
+id, name, position, department, hire_date, salary. 
+
+User Query: "{user_query}" 
+
+Please provide ONLY the MySQL query, with no explanations or context.
+
+"""
+
